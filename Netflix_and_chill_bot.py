729923129f78abf527a49f2f781f22ed33fcc71a
Bernardo <bernardosubercaseaux@gmail.com>
2017-03-07 13:34:08 +0100

    Some functions added and documentation

--- Netflix_and_chill_bot.py
+++ Netflix_and_chill_bot.py
@@ -6,17 +6,38 @@ class Netflix_and_chill_bot(Telegram_bot):
     def __init__(self, Token):
         super(Netflix_and_chill_bot, self).__init__(Token)
         
+	
+	def add_movie(self, bot, update, args):
+		
+		## TO - DO
+		
+		## add_function to actual bot.
+		self.add_function(self.add_movie, "add")
+		
+	def get_movies(self):
+		## TO - DO
+		
+		
+		## add_function to actual bot.
+		self.add_function(self.get_movies, "get")
 
-    
+
+## Initialize bot by http token given by Telegram    
 token = "345755230:AAGtmDv9w6MsDFePYsjUdCvhqObT8-NIPYw"        
 bot = Netflix_and_chill_bot(token)
 
+## Set-up start message (using super-class function)
 start_message = ''' Hi there ! I'm a bot designed for NetflixAndChill's hardest task, choosing what to watch !
         talk to me for help!'''
         
 bot.define_start_message(start_message)
 
 
+## Set-up error handling message (non-existing function called)
 ## ATENTION ! ERROR MESSAGE SHOULD ALWAYS BE AT LAST !
 error_message = "Sorry, I didn't understand that command."
 bot.define_error_message(error_message)
+
+
+## START RUNNING
+bot.run()
--- Telegram_bot.py
+++ Telegram_bot.py
@@ -1,12 +1,15 @@
 class Telegram_bot(object):
 	
+	## Constructor
+	## Issue : What's the correct way of handling imports ?
 	def __init__(self, Token):
 		from telegram.ext import Updater
 		self.updater = Updater(token = Token)
 		self.dispatcher = self.updater.dispatcher
 		import logging
 		logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-     
+    
+    ## Specific start message (Particular case of adding function)
 	def define_start_message(self, message):
 		def start(bot, update):
 			bot.sendMessage(chat_id=update.message.chat_id, text=message)
@@ -14,19 +17,28 @@ class Telegram_bot(object):
 		from telegram.ext import CommandHandler
 		start_handler = CommandHandler('start', start)
 		self.dispatcher.add_handler(start_handler)
-		self.run()
-		
+
+	## Add a function to current bot	
 	def add_function(self, function, name):
 		from telegram.ext import CommandHandler
 		function_handler = CommandHandler(name, function, pass_args=True)
 		self.dispatcher.add_handler(function_handler)
-		self.run()
-
+		
+	## Add message handling, i.e. the bot answer based on messaged and
+	## not on functions.
+	def add_message_handling():
+		return
+		
+	## Start bot !
 	def run(self):
 		self.updater.start_polling()
+		self.updater.idle()
 		
+	## Function to define a message to raise-up when non-existing function
+	## gets called
 	def define_error_message(self, message):
+		from telegram.ext import MessageHandler, Filters
 		def unknown(bot, update):
-			bot.sendMessage(chat_id=update.message.chat_id, text=message)
+			bot.sendMessage(chat_id=update.message.chat_id, text=message	)
 		unknown_handler = MessageHandler(Filters.command, unknown)
 		self.dispatcher.add_handler(unknown_handler)
